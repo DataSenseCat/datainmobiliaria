2761146db4f07efb878feeeae2b4e682b2d4dc56
DataSense <metaworldcups@gmail.com>
2025-08-27 19:41:35 -0300

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -2,11 +2,15 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import './styles/index.css'
+import Layout from './shared/Layout'
 import Home from './pages/Home'
 import Detail from './pages/Detail'
 import Admin from './pages/Admin'
 import Login from './pages/Login'
-import Layout from './shared/Layout'
+import Empresa from './pages/Empresa'
+import Tasaciones from './pages/Tasaciones'
+import Contacto from './pages/Contacto'
+import Emprendimientos from './pages/Emprendimientos'
 
 const router = createBrowserRouter([
   {
@@ -14,7 +18,12 @@ const router = createBrowserRouter([
     element: <Layout/>,
     children: [
       { index: true, element: <Home/> },
+      { path: 'propiedades', element: <Home/> },          // listado reutiliza Home
       { path: 'propiedad/:id', element: <Detail/> },
+      { path: 'emprendimientos', element: <Emprendimientos/> },
+      { path: 'tasaciones', element: <Tasaciones/> },
+      { path: 'empresa', element: <Empresa/> },
+      { path: 'contacto', element: <Contacto/> },
       { path: 'admin', element: <Admin/> },
       { path: 'login', element: <Login/> },
     ]
@@ -22,7 +31,5 @@ const router = createBrowserRouter([
 ])
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <RouterProvider router={router} />
-  </React.StrictMode>,
+  <React.StrictMode><RouterProvider router={router} /></React.StrictMode>,
 )
