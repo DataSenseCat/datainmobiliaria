24f8acb8f095f1eda22c4194eddd49d742802c85
DataSense <metaworldcups@gmail.com>
2025-08-27 19:31:06 -0300

    Create SiteConfig.ts

---
+++ src/shared/SiteConfig.ts
@@ -0,0 +1,8 @@
+export const SITE = {
+  brand: 'Inmobiliaria Catamarca',
+  phone: '+54 383 400-1545',
+  email: 'insumoscatamarca@gmail.com',
+  location: 'Catamarca, Argentina',
+  address: 'Av. República 123, San Fernando del Valle de Catamarca',
+  whatsapp: '+54 383 490-1545',
+}
